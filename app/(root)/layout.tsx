c4067f98bf1a55bc62d3edd9d343e5607f115132
Ajay <exactlyajay.chauhan@gmail.com>
2023-11-27 13:47:02 +0530

    Add sidebar component and update mode toggle button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/navbar";
+import { Sidebar } from "@/components/sidebar";
 
 const RootLayout = async ({
   children
@@ -9,6 +10,10 @@ const RootLayout = async ({
   return ( 
     <div className="h-full">
       <Navbar />
+      <div className="hidden md:flex mt-16 w-20 flex-col
+      fixed insert-y-0">
+        <Sidebar />
+      </div>
       <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
       </div>
       <main className="md:pl-20 pt-16 h-full">
