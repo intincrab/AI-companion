7c5458d13df0aaff07febcab3672e27f49e657b7
Ajay <exactlyajay.chauhan@gmail.com>
2023-11-27 18:53:16 +0530

    Add: cn utility function to layout.tsx and update SearchInput component

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -4,15 +4,15 @@ import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 
 export const SearchInput = () => {
-    return (
-        <div className="relative">
-        <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
-        <Input
+  return (
+    <div className="relative">
+      <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+      <Input
         //   onChange={onChange}
         //   value={value}
-          placeholder="Search..."
-          className="pl-10 bg-primary/10"
-        />
-      </div>
-    )
-}
+        placeholder="Search..."
+        className="pl-10 bg-primary/10"
+      />
+    </div>
+  );
+};
