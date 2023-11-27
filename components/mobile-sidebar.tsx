842a7127ab024851dcde39e01c22891dafc568f1
Ajay <exactlyajay.chauhan@gmail.com>
2023-11-27 18:42:51 +0530

    Update components and dependencies, Add: Search Input

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,7 +1,21 @@
-export const MobileSidebar = () => {
-    return(
-        <div>
-            sidebar
-        </div>
-    )
-}
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sidebar } from "@/components/sidebar";
+
+export const MobileSidebar = ({
+  isPro
+}: {
+  isPro: boolean;
+}) => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+        <Sidebar isPro={isPro} />
+      </SheetContent>
+    </Sheet>
+  );
+};
