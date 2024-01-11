20bc930f9c73130ba5f40fcf66ad0863ac1ef34c
Ajay <exactlyajay.chauhan@gmail.com>
2024-01-11 19:37:33 +0530

    add: the "create" components form

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,7 +1,28 @@
-const CompanionId = () => {
+import prismadb from "@/lib/prismadb";
+import { CompanionForm } from "./components/companion-form";
+
+interface CompanionIdPageProps {
+    params: {
+      companionId: string;
+    };
+  };
+  
+const CompanionIdPage = async ({
+    params
+}: CompanionIdPageProps) => {
+    //TO DO : Check subscription
+    const companion = await prismadb.companion.findUnique({
+        where: {
+            id: params.companionId,
+        }
+    })
+
+    const categories  = await prismadb.category.findMany();
     return (
-        <div>
-            
-        </div>
-    )
+        <CompanionForm
+        initialData={companion}
+        categories={categories}
+        />
+    );
 }
+export default CompanionIdPage;
